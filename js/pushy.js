0360a50d3b0d8d552f5c4e348114dedc065a020f
thomasdeater <thomasdeater@gmail.com>
2019-04-22 11:24:43 -0400

    remove 3dtransform check and refactor toggle logic

--- js/pushy.js
+++ js/pushy.js
@@ -13,6 +13,7 @@
     pushyOpenRight = "pushy-open-right", //css class when menu is open (right position)
     siteOverlay = $(".site-overlay"), //site overlay
     menuBtn = $(".menu-btn, .pushy-link"), //css classes to toggle the menu
+    menuCloseBtn = $(".pushy-close"), //css classes to toggle the menu
     menuBtnFocus = $(".menu-btn"), //css class to focus when menu is closed w/ esc key
     menuLinkFocus = $(pushy.data("focus")), //focus on link when menu is open
     menuSpeed = 200, //jQuery fallback menu speed
@@ -20,88 +21,47 @@
     submenuClass = ".pushy-submenu",
     submenuOpenClass = "pushy-submenu-open",
     submenuClosedClass = "pushy-submenu-closed",
-    submenu = $(submenuClass);
-
-  //close menu w/ esc key
-  $(document).keyup(function(e) {
-    //check if esc key is pressed
-    if (e.keyCode == 27) {
-      //check if menu is open
-      if (body.hasClass(pushyOpenLeft) || body.hasClass(pushyOpenRight)) {
-        if (cssTransforms3d) {
-          closePushy(); //close pushy
-        } else {
-          closePushyFallback();
-          opened = false; //set menu state
-        }
-
-        //focus on menu button after menu is closed
-        if (menuBtnFocus) {
-          menuBtnFocus.focus();
-        }
-      }
-    }
-  });
+    submenu = $(submenuClass),
+    opened = false;
 
   function togglePushy() {
-    //add class to body based on menu position
-    if (pushy.hasClass(pushyLeft)) {
-      body.toggleClass(pushyOpenLeft);
+    if (opened) {
+      closePushy();
     } else {
-      body.toggleClass(pushyOpenRight);
-    }
-
-    //focus on link in menu after css transition ends
-    if (menuLinkFocus) {
-      pushy.one("transitionend", function() {
-        menuLinkFocus.focus();
-      });
+      openPushy();
     }
   }
 
   function closePushy() {
+    opened = false;
     if (pushy.hasClass(pushyLeft)) {
       body.removeClass(pushyOpenLeft);
     } else {
       body.removeClass(pushyOpenRight);
     }
-    console.log("pushy closed!");
+
+    //focus on menu button after menu is closed
+    // TODO return focus to activeElement
+    // TODO aria-hidden stuff
+    if (menuBtnFocus) {
+      menuBtnFocus.focus();
+    }
   }
 
-  function openPushyFallback() {
-    //animate menu position based on CSS class
+  function openPushy() {
+    opened = true;
     if (pushy.hasClass(pushyLeft)) {
       body.addClass(pushyOpenLeft);
-      pushy.animate({ left: "0px" }, menuSpeed);
-      container.animate({ left: menuWidth }, menuSpeed);
-      //css class to add pushy capability
-      push.animate({ left: menuWidth }, menuSpeed);
     } else {
       body.addClass(pushyOpenRight);
-      pushy.animate({ right: "0px" }, menuSpeed);
-      container.animate({ right: menuWidth }, menuSpeed);
-      push.animate({ right: menuWidth }, menuSpeed);
     }
 
-    //focus on link in menu
+    //focus on link in menu after css transition ends
+    // TODO aria-hidden stuff
     if (menuLinkFocus) {
-      menuLinkFocus.focus();
-    }
-  }
-
-  function closePushyFallback() {
-    //animate menu position based on CSS class
-    if (pushy.hasClass(pushyLeft)) {
-      body.removeClass(pushyOpenLeft);
-      pushy.animate({ left: "-" + menuWidth }, menuSpeed);
-      container.animate({ left: "0px" }, menuSpeed);
-      //css class to add pushy capability
-      push.animate({ left: "0px" }, menuSpeed);
-    } else {
-      body.removeClass(pushyOpenRight);
-      pushy.animate({ right: "-" + menuWidth }, menuSpeed);
-      container.animate({ right: "0px" }, menuSpeed);
-      push.animate({ right: "0px" }, menuSpeed);
+      pushy.one("transitionend", function() {
+        menuLinkFocus.focus();
+      });
     }
   }
 
@@ -129,93 +89,29 @@
     });
   }
 
-  //checks if 3d transforms are supported removing the modernizr dependency
-  var cssTransforms3d = (function csstransforms3d() {
-    var el = document.createElement("p"),
-      supported = false,
-      transforms = {
-        webkitTransform: "-webkit-transform",
-        OTransform: "-o-transform",
-        msTransform: "-ms-transform",
-        MozTransform: "-moz-transform",
-        transform: "transform"
-      };
+  //toggle submenu
+  toggleSubmenu();
 
-    if (document.body !== null) {
-      // Add it to the body to get the computed style
-      document.body.insertBefore(el, null);
+  //toggle menu
+  menuBtn.on("click", function() {
+    togglePushy();
+  });
 
-      for (var t in transforms) {
-        if (el.style[t] !== undefined) {
-          el.style[t] = "translate3d(1px,1px,1px)";
-          supported = window
-            .getComputedStyle(el)
-            .getPropertyValue(transforms[t]);
-        }
-      }
+  //close menu when clicking site overlay or close button
+  menuCloseBtn.on("click", function() {
+    closePushy();
+  });
+  siteOverlay.on("click", function() {
+    closePushy();
+  });
 
-      document.body.removeChild(el);
-
-      return (
-        supported !== undefined && supported.length > 0 && supported !== "none"
-      );
-    } else {
-      return false;
-    }
-  })();
-
-  if (cssTransforms3d) {
-    //toggle submenu
-    toggleSubmenu();
-
-    //toggle menu
-    menuBtn.on("click", function() {
-      togglePushy();
-    });
-    //close menu when clicking site overlay
-    siteOverlay.on("click", function() {
-      togglePushy();
-    });
-  } else {
-    //add css class to body
-    body.addClass("no-csstransforms3d");
-
-    //hide menu by default
-    if (pushy.hasClass(pushyLeft)) {
-      pushy.css({ left: "-" + menuWidth });
-    } else {
-      pushy.css({ right: "-" + menuWidth });
-    }
-
-    //fixes IE scrollbar issue
-    container.css({ "overflow-x": "hidden" });
-
-    //keep track of menu state (open/close)
-    var opened = false;
-
-    //toggle submenu
-    toggleSubmenu();
-
-    //toggle menu
-    menuBtn.on("click", function() {
+  //close menu w/ esc key
+  $(document).keyup(function(e) {
+    //check if esc key is pressed
+    if (e.keyCode == 27) {
       if (opened) {
-        closePushyFallback();
-        opened = false;
-      } else {
-        openPushyFallback();
-        opened = true;
+        closePushy();
       }
-    });
-
-    //close menu when clicking site overlay
-    siteOverlay.on("click", function() {
-      if (opened) {
-        closePushyFallback();
-        opened = false;
-      } else {
-        openPushyFallback();
-        opened = true;
-      }
-    });
-  }
+    }
+  });
 })(jQuery);
